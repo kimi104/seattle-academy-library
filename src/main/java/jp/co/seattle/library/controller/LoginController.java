5560a9eaa99cfb9f2e70ecb5e04a80b065fc161a
sakatakimika <sakatakimika@192.168.130.213>
2023-04-11 18:21:15 +0900

    task2

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -42,8 +42,13 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
+		if (selectedUserInfo != null) {
+			return "redirect:/home";
+		} else {
+			model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません");
+			return "createAccount";
 
-		
-		return "redirect:/home";
+		}
+
 	}
 }
