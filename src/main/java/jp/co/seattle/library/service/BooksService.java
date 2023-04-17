c81ccc85b34825238b478fc752ba00632d1a350b
sakatakimika <sakatakimika@192.168.130.213>
2023-04-17 12:46:26 +0900

    task5

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -99,5 +99,6 @@ public class BooksService {
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		}
+
 	}
 }
